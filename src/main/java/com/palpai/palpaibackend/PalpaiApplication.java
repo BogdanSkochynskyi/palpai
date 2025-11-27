73b47600e62801ceddd73b816b0907cd04ea756b
BTRco <bogdanskochynskyi@gmail.com>
2025-11-27 15:41:02 +0200

    Init commit

---
+++ src/main/java/com/palpai/palpaibackend/PalpaiApplication.java
@@ -0,0 +1,13 @@
+package com.palpai.palpaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalpaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PalpaiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/palpai/palpaibackend/PalpaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.palpai.palpaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalpaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
